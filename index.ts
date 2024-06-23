1957ec190e3e2fca505983903c65076fd0875946
Muhammad Muzzamil Bukhari <muhammadmuzzmilbukhari@gmail.com>
2024-06-23 12:54:18 +0500

    adding cli todo list

---
+++ index.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let todos: string[] = [];
+
+let loop = true;
+console.log("\t Welcome in TODO List \t\n");
+
+while (loop) {
+  let ans = await inquirer.prompt([
+    {
+      type: "input",
+      name: "todoMsg",
+      message: "What do you want to add in todo? ",
+    },
+    {
+      type: "confirm",
+      name: "addMore",
+      message: "Do you want to Add more? ",
+      default: false,
+    },
+  ]);
+  const { todoMsg, addMore } = ans;
+  loop = addMore;
+  if (todoMsg) {
+    todos.push(todoMsg);
+  } else {
+    console.log("Please add valid input!");
+  }
+}
+if (todos.length > 0) {
+  console.log("\n\tAll todos : \t\n");
+
+  todos.forEach((todo) => console.log(todo));
+} else {
+  console.log("No todos found!");
+}
